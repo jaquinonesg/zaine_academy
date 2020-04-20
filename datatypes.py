ed8107a935334a14f0d8ca13fb78451e2282643e
Jorge Quiñones <jaquinonesg@gmail.com>
2020-04-20 08:59:36 -0500

    Added datatypes

---
+++ datatypes.py
@@ -0,0 +1,47 @@
+# Text type
+StringA = "Soy una cadena"
+
+# Numeric type
+IntegerA = 100
+IntegerB = 3
+FloatA = 20.5
+ComplexA = 20j
+
+# Basic operations
+# Addition, Substraction, Multiplication, Division, Modulus, Exponent, Floor Divition
+
+# print(IntegerA/IntegerB)
+# print(IntegerA//IntegerB)
+# print(IntegerA%IntegerB)
+# print(IntegerA**IntegerB)
+
+# Casting types
+numA = int("100")
+numB = int("20")
+# print(numA*numB)
+# print(type(FloatA))
+
+# Others
+#Boolean Type: bool
+BoolA = False
+BoolB = True
+
+#Sequence types: Tuple, Range, List
+TupleA =  (1,2,3,4,5)
+RangeA = range(8)
+ListA = [1,2,3,4,5]
+print(type(TupleA))
+print(type(RangeA))
+print(type(ListA[1]))
+
+#Mapping type: Dict
+DictA = {
+    "key" : "value",
+    "anotherkey" : "anothervalue"
+}
+print(DictA)
+
+#Set types: set, frozenset
+
+#Binary types: bytes, bytearray, memoryview
+
